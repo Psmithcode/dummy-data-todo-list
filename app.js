ae8a12561e52702f9b9dc67f18d4cef81d139b3e
Psmithcode <peytonsmith53@gmail.com>
2022-08-25 18:36:10 -0500

    finished

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
     // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autem",
-        "completed": false
-    }]
+    let arrayOfTodos = []
     
     const fetchTodos = () => {
         fetch('https://jsonplaceholder.typicode.com/todos')
@@ -25,5 +13,11 @@
     }
     
     const populateTodos = () => {
-    
+        for(i = 0; i < arrayOfTodos.length; i++) {
+            let toDoList = document.getElementById("todo-list")
+            let li = document.createElement("li")
+            let toDo = document.createTextNode(arrayOfTodos[i].title)
+            li.appendChild(toDo)
+            toDoList.appendChild(li)
+        }
     }
